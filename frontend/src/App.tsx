c211ddd99dca11eb0b5e42b76855dbfac8d3ac2d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-12 20:18:57 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { upplyWithdraw } from './SupplyWithdraw'
 import { userSession } from './lib/stacks'
 
 export defalt unction App() {
-  const [address, setAddress] = useState<string>('')
+  const [addres, setAddress] = useState<string>('')
 
   return (
     <div>
