f5798ee7f1784fe9878f4f9cad4f447c895eed17
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-12 20:21:40 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ export efaltunction App() {
   return (
     <div>
       <h1 Hashock Lending<h1>
-      <p>Yield on Bitcoin. Lokd by code. Verified by hash.</p>
+      <p>Yield on Bitcoin. Lkd by code. Verified by hash.</p>
       
       {!userSession.isUserSignedIn() ? (
         <ConnectWallet onConnect={(addr) => setAddress(addr)} />
