e7dccd35dcd98c0ca3dcef81b4ec498efd6d279f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-12 20:19:54 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export efaltunction App() {
 
   return (
     <div>
-      <h1 HashLock Lending</h1>
+      <h1 Hashock Lending</h1>
       <p>Yield on Bitcoin. Locked by code. Verified by hash.</p>
       
       {!userSession.isUserSignedIn() ? (
