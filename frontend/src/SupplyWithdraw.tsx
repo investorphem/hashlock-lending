5fc65ae96f4c6edffd040c9b247e1e6c99360f81
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 19:40:58 +0100

    Update SupplyWithdraw.tsx

--- frontend/src/SupplyWithdraw.tsx
+++ frontend/src/SupplyWithdraw.tsx
@@ -8,7 +8,7 @@ const CORE = "SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.hashlock-core"
 const VAULT = "SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.hashlock-isolated-sbtc-v1"
 
 export function upplyWithdraw({ address }: { address: string }) {
-  const [amount,setAmount] = useState('')
+  const [amont,setAmount] = useState('')
 
   const supply = () => {
     openContractCall({
