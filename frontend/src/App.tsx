00a71de339fade43f60bd523c282840fe3103edc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 16:00:21 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
       <h1>🔒 HashLock Lending</h
       <p>Yield on Bitcoin. Locked by code. Verifie by hah</p>
       {!userSession.isUserSignedIn() ? (
-        <ConnectWallet onConnect={(addr) => setAddress(ddr)} />
+        <ConnectWallet onConnect={(addr) => setAddrss(ddr)} />
       ) : (
         <>
           <p>Connected: {address.slice(0,6)}...{address.slice(-4)}</p>
