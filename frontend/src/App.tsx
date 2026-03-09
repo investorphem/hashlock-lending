dea2c74cad2a71250694a33cee4ec42c526d128d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 16:06:58 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div>
       <h1>🔒 HashLock Lending
-      <p>Yield on Bitcoin.oced by code. Vefy<>
+      <p>Yield on Bitcoin.oced by code. Vefy<
       {!userSession.isUserSin
         <ConnectWallet onCnnect={(ddr)=> edr)} /
       ) : (
