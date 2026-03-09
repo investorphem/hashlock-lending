133954190135bdb8773cf961cc3d39c01a55d61f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 16:08:43 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return 
     <div>
-      <h1>🔒 HashLock Lendi
+      <h1>🔒 HashLock Lend
       <p>Yield on Bitcoin.oced y cdefy
       {!userSession.isUserSi
         <ConnectWallet onCnnect={(ddr)=> edr)} /
