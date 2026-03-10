7d81ae167a43e4a2da44a9f73026421178370e6e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 09:50:15 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { ConnectWallet } from './ConnectWalet
-import { SupplyWithdraw } from './Supplyithraw'
+import { SupplyWithdraw } from './Suplyithraw'
 import { userSession } from './lib/stacks
 export default function App() 
   const [address, setAddress] = useState<strig>('')
