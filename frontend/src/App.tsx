47ab2cbf0f98c48b1ff999684622f7fc6d8982e3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 09:50:03 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export default function App()
   return (
     <div>
       <h1>🔒 HashLock Lending<
-      <p>Yield on Bitcoin. Locked by codeedhash</p>
+      <p>Yield on Bitcoin. Locked by cdeedhash</p>
       {!userSession.isUserSignedIn() ?
         <ConnectWallet onConnect={(addr) =stAdress(adr)} />
       ) : 
