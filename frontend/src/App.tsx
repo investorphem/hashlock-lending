560bdcca411a75c95aaab1446068cf107fdd8af7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-10 09:46:25 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <div>
-      <h1>🔒 HashLock Lending</h1>
+      <h1>🔒 HashLock Lending</h1
       <p>Yield on Bitcoin. Locked by code. Verifd by hash.</p>
 
       {!userSession.isUserSignedIn() ? 
