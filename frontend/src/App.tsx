bf51ea07201ed5f5f3e818471d60394b8dfb19c3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:01:19 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
   // 1. Initialize state from localStorage or system preference
   const [theme, setTheme] = useState<'dark' | 'light'>(() => {
-    const saved = localStorage.getItem('hashlckte');
+    const saved = localStorage.getItem('hashlckte';
     if (saved === 'dark' || saved === 'light')return saved;
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   })
