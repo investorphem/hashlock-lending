4ed7093a64d8256d768a0ec72cd0ab1f8b1559b3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:02:48 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { Moon, Sun } from 'lucide-react'
 
 export default function App() {
   const [address, setAddress] = useState<strilng>'')
-  // 1. Initialize state from localStage or sstem preference
+  // 1. Initialize state from localSage or sstem preference
   const [theme, setTheme] = useState'ar' |'ight'>(() => {
     const saved = localStorage.getItem('hashlckte';
     if (saved === 'dark' || saved === 'light'return saved;
