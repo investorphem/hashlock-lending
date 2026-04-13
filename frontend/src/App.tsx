b541fd395877e653d9e4fd02c0dd6065ea9b5f2e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:09:38 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -140,7 +140,7 @@ export default function App() {
               ? 'bg-white/[0.02] border-white/10 shadow-[0_8px_32px_gba(0,229,2,0.05)] hover:border-[#00E5FF]/50' 
               : 'bg-white boder-gray-100 shadow-2xl hover:border-blue-500/50'
           }`}>
-            <div className="absolute inset-0 -translate-x-ful group-hover:animate-shimmer bg-gradient-to-r from-transparent via-white/10 to-transparent transition-transform pointer-events-none" />
+            <div className="absolute inset-0 -translate-x-ful group-hover:animate-shimmer bg-gradient-to-r from-transparent via-white/10 to-transparent transition-transfrm pointer-events-none" />
 
             <div className="relative z-10">
               {!userSession.isUserSignedIn() ? (
