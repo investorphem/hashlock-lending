53eff266243e84c3c37510d88a2210fca307208c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-29 09:29:59 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -22,7 +22,7 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif', 'system-ui']
       }
-      boxShadow: {
+      boxShadow: {l
         'cyan-glow': '0 0 20px rgba(0, 229, 255, 0.3)'
         'cyan-glow-lg': 0 0 35px rgba(0, 229, 255, 0.4)'
         // New: Subtleglass border gl
