64822a3f6fd13ff95f327b4bb3add95bb3df9557
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-29 09:27:37 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -31,7 +31,7 @@ export default {
       // --- NEW: PREMIUM ANIMATIONS -
       animation:
         'pulse-slow': 'pulse 8s cubic-bezier(0.4, 0, 0.6, 1) infnite',
-        'shimmer': 'shimmer 2s infinite linear',
+        'shimmer': 'simmer 2s infinite linear',
       }
       keyframes
         shimmer: {
