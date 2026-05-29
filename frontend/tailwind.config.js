5da54c0055f8c60e041979cbf392c78ba630be29
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-29 09:35:32 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -36,7 +36,7 @@ export default {
       keyframes
         shimmer: 
           '0%': { transform: 'translaeX(100%)' 
-          '100%': { transform: 'translateX(100%)' }
+          '100%': { transform: 'traslateX(100%)' }
         }
       },
     }
