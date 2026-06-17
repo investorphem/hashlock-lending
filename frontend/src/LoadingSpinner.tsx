ba91dd466aebcd7ba958acd68e140075e3866c48
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-17 09:41:58 +0100

    Update LoadingSpinner.tsx

--- frontend/src/LoadingSpinner.tsx
+++ frontend/src/LoadingSpinner.tsx
@@ -8,13 +8,6 @@ export const LoadingSpinner = ({ className = "w-5 h-5" }: { className?: string }
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Background Hexagon Path */}
-      <path 
-        d="M50 5L89 27.5V72.5L50 95L11 72.5V27.5L50 5Z" 
-        stroke="currentColor" 
-        strokeWidth="8" 
-        strokeOpacity="0.2"
-      />
       {/* The "Loading" Segment */}
       <path 
         d="M50 5L89 27.5V40" 
