d439a1db31c179b17cbbc72933afcd9e90bfaae1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-18 09:06:45 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 // This is crucial: It loads your Tailwind styles and premium base fonts!
-import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
