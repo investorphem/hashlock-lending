4bdb9820f0bd698a62e0327c63f0d31bd5155b14
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-27 08:29:21 +0100

    Update TransactionHistory.tsx

--- frontend/src/TransactionHistory.tsx
+++ frontend/src/TransactionHistory.tsx
@@ -14,7 +14,6 @@ interface TransactionHistoryProps {
 }
 
 export function TransactionHistory({ address, theme = 'dark' }: TransactionHistoryProps) {
-  const [transactions, setTransactions] = useState<Tx[]>([])
 
         // Fetch last 5 transactions for this user from Hiro API
         const res = await fetch(`https://api.mainnet.hiro.so/extended/v1/address/${address}/transactions?limit=5`)
