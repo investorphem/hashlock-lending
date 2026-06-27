5195becf348727da14fa7fe709ed80b3cb6a6681
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-27 08:31:57 +0100

    Update stacks.ts

--- frontend/src/lib/stacks.ts
+++ frontend/src/lib/stacks.ts
@@ -1,9 +1,7 @@
 import { AppConfig, UserSession } from '@stacks/connect'
 // ==========================================
 // NETWORK CONFIGURATIO
-// ==========================================
-// AUTHENTICATION & SESSION
-// ==========================================
+// =========================================
 // 'store_write' and 'publish_data' permissions are perfect 
 // for interacting with your decentralized lending vaults.
 const appConfig = new AppConfig(['store_write', 'publish_data'])
