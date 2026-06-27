8896bedbf0fedfcff36c2bf900a24df24118a18c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-27 08:34:16 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,4 @@ import App from './App'
 import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <React.StrictMo
