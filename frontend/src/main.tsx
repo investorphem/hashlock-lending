b1bbd67f631b529dc6e84221fc02d9eea27c05d8
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-27 08:35:08 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
 // This is crucial: It loads your Tailwind styles and premium base fonts!
 import './index.css' 
 
